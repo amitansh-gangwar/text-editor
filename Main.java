921f56e4f62f175601eebf5cadf41f0c25ed2fd6
amitansh-gangwar <amitansh.g.aux@go-jek.com>
2022-12-10 17:52:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
-import operations.*;
-
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.*;
@@ -244,5 +243,104 @@ class Main extends JFrame implements ActionListener, ItemListener {
         String fontType = (String) fontName.getSelectedItem();
         int font = Integer.valueOf((String) fontSize.getSelectedItem());
         textArea.setFont(new Font(fontType, Font.PLAIN, font));
+    }
+
+    public static class CopyOperation implements Operation{
+        @Override
+        public void doOperation(JTextArea textArea, JFrame frame) {
+            textArea.copy();
+        }
+    }
+
+    public static class CutOperation implements Operation{
+        @Override
+        public void doOperation(JTextArea textArea, JFrame frame) {
+            textArea.cut();
+        }
+    }
+
+    public static class LoadOperation implements Operation{
+        @Override
+        public void doOperation(JTextArea textArea, JFrame frame) {
+            JFileChooser j = new JFileChooser("f:");
+
+            // Invoke the showsOpenDialog function to show the save dialog
+            int r = j.showOpenDialog(null);
+
+            // If the user selects a file
+            if (r == JFileChooser.APPROVE_OPTION) {
+                // Set the label to the path of the selected directory
+                File fi = new File(j.getSelectedFile().getAbsolutePath());
+
+                try {
+                    // String
+                    String s1 = "", sl = "";
+
+                    // File reader
+                    FileReader fr = new FileReader(fi);
+
+                    // Buffered reader
+                    BufferedReader br = new BufferedReader(fr);
+
+                    // Initialize sl
+                    sl = br.readLine();
+
+                    // Take the input from the file
+                    while ((s1 = br.readLine()) != null) {
+                        sl = sl + "\n" + s1;
+                    }
+
+                    // Set the text
+                    textArea.setText(sl);
+                }
+                catch (Exception evt) {
+    //                JOptionPane.showMessageDialog(f, evt.getMessage());
+                }
+            }
+        }
+    }
+
+    public static interface Operation {
+        public void doOperation(JTextArea textArea, JFrame frame);
+    }
+
+    public static class PasteOperation implements Operation {
+        @Override
+        public void doOperation(JTextArea textArea, JFrame frame) {
+            textArea.paste();
+        }
+    }
+
+    public static class SaveOperation implements Operation {
+        @Override
+        public void doOperation(JTextArea textArea, JFrame frame) {
+            JFileChooser j = new JFileChooser("f:");
+
+            // Invoke the showsSaveDialog function to show the save dialog
+            int r = j.showSaveDialog(null);
+
+            if (r == JFileChooser.APPROVE_OPTION) {
+
+                // Set the label to the path of the selected directory
+                File fi = new File(j.getSelectedFile().getAbsolutePath());
+
+                try {
+                    // Create a file writer
+                    FileWriter wr = new FileWriter(fi, false);
+
+                    // Create buffered writer to write
+                    BufferedWriter w = new BufferedWriter(wr);
+
+                    // Write
+                    w.write(textArea.getText());
+
+                    w.flush();
+                    w.close();
+                }
+                catch (Exception evt) {
+    //                JOptionPane.showMessageDialog(f, evt.getMessage());
+                }
+            }
+        }
     }
 }
